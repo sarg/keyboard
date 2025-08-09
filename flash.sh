4fc959767617d84a31a6f7d19514934d5fe68db6
Sergey Trofimov <sarg@sarg.org.ru>
2025-08-09 12:30:24 +0200

    Bump qmk

--- flash.sh
+++ flash.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env -S guix shell dfu-programmer -- sh
+
+set -e
+FIRMWARE=$(guix build -f guix.scm)/sarg_default.hex
+test -r $FIRMWARE
+
 dfu-programmer atmega32u4 erase
-dfu-programmer atmega32u4 flash $(guix build -f guix.scm)/sarg_default.hex
+dfu-programmer atmega32u4 flash $FIRMWARE
 dfu-programmer atmega32u4 launch
--- guix.scm
+++ guix.scm
@@ -1,8 +1,24 @@
 (use-modules (gnu packages firmware)
              (guix utils)
+             (guix packages)
+             (guix git-download)
              (guix gexp))
 
-(make-qmk-firmware "sarg" "default"
-                   #:description "Firmware for my keyb"
-                   #:keyboard-source-directory
-                   (local-file "./firmware" #:recursive? #t))
+(let ((base
+       (make-qmk-firmware "sarg" "default"
+                          #:description "Firmware for my keyb"
+                          #:keyboard-source-directory
+                          (local-file "./firmware" #:recursive? #t))))
+  (package
+    (inherit base)
+    (name "qmk-firmware-sarg")
+    (version "0.29.12")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/qmk/qmk_firmware")
+                     (commit version)))
+              (file-name (git-file-name "qmk-firmware" version))
+              (sha256
+               (base32
+                "16rmyjhxgpiqkkfkk5v5ighzwww2d96gdjiwq0wgz1fkmdxfj2ik"))))))
